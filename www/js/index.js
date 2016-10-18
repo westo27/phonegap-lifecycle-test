d3c581b36ae9dc8816e3fac2f98dff1d051115ad
Tom Weston <west1_15@uni.worc.ac.uk>
2016-10-18 10:05:16 +0100

    Change to Data Storage and Retrieval Lab Session

--- www/js/index.js
+++ www/js/index.js
@@ -1,41 +1,15 @@
-var paused_count =0;
-var resumed_count = 0;
-var launched_count = 0;
+window.localStorage.setItem(bottle, 500ml);
+window.localStorage.setItem(notepad, writing);
+window.localStorage.setItem(pen, blue);
+window.localStorage.setItem(computer, dell);
+window.localStorage.setItem(phone, motorola);
 
-    function onLoad() { //handler
-        document.addEventListener("deviceready", onDeviceReady, false); //event listener
-		console.log("device ready");
-    }
-	
-	function updateDisplay() {
-		$("#launched").text("Application launched: " + launched_count);
-		$("#resumed").text("Application paused: " + paused_count);
-		$("#paused").text("Application resumed: " + resumed_count);
-	}
+var phone = window.localStorage.getItem(phone);
 
+//simple
+window.alert(phone);
 
-    // device APIs are available
-    //
-    function onDeviceReady() { //handler
-		alert("device ready");
-        
-		document.addEventListener("resume", onResume, false); //event listener
-		document.addEventListener("pause", onPause, false); //event listener
-		
-		launched_count++;
-		updateDisplay();
-    }
-
-    // Handle the pause event
-    //
-    function onPause() { //handler
-		alert("pause");
-		paused_count++;
-		updateDisplay();
-    }
-	
-	function onResume() { //handler
-		alert("resume");
-		resumed_count++;
-		updateDisplay();
-    }
+//better
+$(function () {          
+    $("#myText").text("hoover");
+});
