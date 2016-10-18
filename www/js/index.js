a358b58451df6bed7d6cf5d3e2e767a3e03d9975
Tom Weston <west1_15@uni.worc.ac.uk>
2016-10-18 10:21:59 +0100

    value

--- www/js/index.js
+++ www/js/index.js
@@ -1,15 +1,20 @@
-window.localStorage.setItem(bottle, 500ml);
-window.localStorage.setItem(notepad, writing);
-window.localStorage.setItem(pen, blue);
-window.localStorage.setItem(computer, dell);
-window.localStorage.setItem(phone, motorola);
+//window.localStorage.setItem(bottle, ml);
+//window.localStorage.setItem(notepad, writing);
+//window.localStorage.setItem(pen, blue);
+//window.localStorage.setItem(computer, dell);
+//window.localStorage.setItem(phone, motorola);
 
-var phone = window.localStorage.getItem(phone);
+var key = "pen";
+var value = "blue";
+
+window.localStorage.setItem( key, value );
+
+var value = window.localStorage.getItem(key);
 
 //simple
-window.alert(phone);
+window.alert(value);
 
 //better
 $(function () {          
-    $("#myText").text("hoover");
+    $("#myText").text(value);
 });
